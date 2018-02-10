9ade31c31c24c9ab87e8c85026bb78d661d42af0
zhouhuafei <1123486116@qq.com>
2018-02-10 22:23:34 +0800

    1.0.1

--- dist/index.js
+++ dist/index.js
@@ -2,152 +2,170 @@
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var Super = function () {
-    function Super() {
-        _classCallCheck(this, Super);
-
-        this.event = {};
-        this.eventOne = {};
+(function (name, factory) {
+    if ((typeof exports === 'undefined' ? 'undefined' : _typeof(exports)) === 'object' && typeof module !== 'undefined') {
+        // nodejs - commonjs canon
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) {
+        // requirejs - amd canon
+        define(factory);
+    } else if (window) {
+        // window - browser canon
+        if (Object.prototype.toString.call(window.zhf).slice(8, -1).toLowerCase() !== 'Object') {
+            window.zhf = {};
+        }
+        window.zhf[name] = factory();
     }
+})('EventEmitter', function () {
+    var EventEmitter = function () {
+        function EventEmitter() {
+            _classCallCheck(this, EventEmitter);
 
-    _createClass(Super, [{
-        key: '_bind',
-        value: function _bind(str, fn) {
-            var minNum = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1;
-            var event = arguments[3];
+            this.event = {};
+            this.eventOne = {};
+        }
 
-            var obj = event[str];
-            if (!obj) {
-                obj = [];
-                event[str] = obj;
-            }
-            obj.push({
-                isDel: false,
-                minNum: minNum,
-                triggerNum: 0,
-                fn: fn
-            });
-        }
-    }, {
-        key: '_cancel',
-        value: function _cancel(str, num, event) {
-            var obj = event[str];
-            if (obj) {
-                if (num && num >= 1) {
-                    obj.splice(num - 1, 1, {
-                        isDel: true,
-                        fn: function fn() {}
-                    });
-                } else {
-                    obj.length = 0;
+        _createClass(EventEmitter, [{
+            key: '_bind',
+            value: function _bind(str, fn) {
+                var minNum = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 1;
+                var event = arguments[3];
+
+                var obj = event[str];
+                if (!obj) {
+                    obj = [];
+                    event[str] = obj;
                 }
-            }
-        }
-    }, {
-        key: '_cancelAll',
-        value: function _cancelAll(event) {
-            Object.keys(event).forEach(function (key) {
-                event[key].length = 0;
-            });
-        }
-    }, {
-        key: '_trigger',
-        value: function _trigger(str, data, cb, event, type) {
-            var obj = event[str];
-            if (obj) {
-                if (!obj.allData) {
-                    obj.allData = [];
-                }
-                obj.allData.push(data);
-                obj.forEach(function (json) {
-                    json.triggerNum++;
-                    if (json.triggerNum >= json.minNum && json.isDel === false) {
-                        json.fn({ nowData: data, allData: obj.allData });
-                        // 销毁发布过的单次订阅
-                        if (type === 'once') {
-                            json.isDel = true;
-                            json.fn = function () {};
-                        }
-                    }
-                    if (Object.prototype.toString.call(cb).slice(8, -1).toLowerCase() === 'function') {
-                        json.data = data;
-                        cb(json);
-                    }
+                obj.push({
+                    isDel: false,
+                    minNum: minNum,
+                    triggerNum: 0,
+                    fn: fn
                 });
-                // 销毁全部单次订阅
-                if (type === 'once') {
-                    var isDelAll = true;
-                    obj.forEach(function (json) {
-                        if (json.isDel === false) {
-                            isDelAll = false;
-                        }
-                    });
-                    if (isDelAll) {
+            }
+        }, {
+            key: '_cancel',
+            value: function _cancel(str, num, event) {
+                var obj = event[str];
+                if (obj) {
+                    if (num && num >= 1) {
+                        obj.splice(num - 1, 1, {
+                            isDel: true,
+                            fn: function fn() {}
+                        });
+                    } else {
                         obj.length = 0;
                     }
                 }
             }
-        }
-
-        // 订阅 minNum - 至少发布(emit)多少次才会发消息(on)给订阅者
-
-    }, {
-        key: 'on',
-        value: function on(str, fn, minNum) {
-            this._bind(str, fn, minNum, this.event);
-        }
-
-        // 单次订阅 minNum - 至少发布(emit)多少次才会发消息(on)给订阅者
-
-    }, {
-        key: 'once',
-        value: function once(str, fn, minNum) {
-            this._bind(str, fn, minNum, this.eventOne);
-        }
-
-        // 取消订阅
-
-    }, {
-        key: 'off',
-        value: function off(str, num) {
-            if (str) {
-                this._cancel(str, num, this.event);
-                this._cancel(str, num, this.eventOne); // 取消单次订阅
-            } else {
-                this._cancelAll(this.event);
-                this._cancelAll(this.eventOne); // 取消全部单次订阅
+        }, {
+            key: '_cancelAll',
+            value: function _cancelAll(event) {
+                Object.keys(event).forEach(function (key) {
+                    event[key].length = 0;
+                });
             }
-        }
-
-        // 发布
-
-    }, {
-        key: 'emit',
-        value: function emit(str, data, cb) {
-            this._trigger(str, data, cb, this.event);
-            this._trigger(str, data, cb, this.eventOne, 'once'); // 发布单次订阅并销毁
-        }
-
-        // 数组转对象
-
-    }, {
-        key: 'arrToJson',
-        value: function arrToJson(arr) {
-            var json = {};
-            arr.forEach(function (v, i) {
-                if (v.name) {
-                    json[v.name] = v;
-                } else {
-                    json[i] = v;
+        }, {
+            key: '_trigger',
+            value: function _trigger(str, data, cb, event, type) {
+                var obj = event[str];
+                if (obj) {
+                    if (!obj.allData) {
+                        obj.allData = [];
+                    }
+                    obj.allData.push(data);
+                    obj.forEach(function (json) {
+                        json.triggerNum++;
+                        if (json.triggerNum >= json.minNum && json.isDel === false) {
+                            json.fn({ nowData: data, allData: obj.allData });
+                            // 销毁发布过的单次订阅
+                            if (type === 'once') {
+                                json.isDel = true;
+                                json.fn = function () {};
+                            }
+                        }
+                        if (Object.prototype.toString.call(cb).slice(8, -1).toLowerCase() === 'function') {
+                            json.data = data;
+                            cb(json);
+                        }
+                    });
+                    // 销毁全部单次订阅
+                    if (type === 'once') {
+                        var isDelAll = true;
+                        obj.forEach(function (json) {
+                            if (json.isDel === false) {
+                                isDelAll = false;
+                            }
+                        });
+                        if (isDelAll) {
+                            obj.length = 0;
+                        }
+                    }
                 }
-            });
-            return json;
-        }
-    }]);
+            }
 
-    return Super;
-}();
+            // 订阅 minNum - 至少发布(emit)多少次才会发消息(on)给订阅者
 
-module.exports = Super;
+        }, {
+            key: 'on',
+            value: function on(str, fn, minNum) {
+                this._bind(str, fn, minNum, this.event);
+            }
+
+            // 单次订阅 minNum - 至少发布(emit)多少次才会发消息(on)给订阅者
+
+        }, {
+            key: 'once',
+            value: function once(str, fn, minNum) {
+                this._bind(str, fn, minNum, this.eventOne);
+            }
+
+            // 取消订阅
+
+        }, {
+            key: 'off',
+            value: function off(str, num) {
+                if (str) {
+                    this._cancel(str, num, this.event);
+                    this._cancel(str, num, this.eventOne); // 取消单次订阅
+                } else {
+                    this._cancelAll(this.event);
+                    this._cancelAll(this.eventOne); // 取消全部单次订阅
+                }
+            }
+
+            // 发布
+
+        }, {
+            key: 'emit',
+            value: function emit(str, data, cb) {
+                this._trigger(str, data, cb, this.event);
+                this._trigger(str, data, cb, this.eventOne, 'once'); // 发布单次订阅并销毁
+            }
+
+            // 数组转对象
+
+        }, {
+            key: 'arrToJson',
+            value: function arrToJson(arr) {
+                var json = {};
+                arr.forEach(function (v, i) {
+                    if (v.name) {
+                        json[v.name] = v;
+                    } else {
+                        json[i] = v;
+                    }
+                });
+                return json;
+            }
+        }]);
+
+        return EventEmitter;
+    }();
+
+    return EventEmitter;
+});
--- src/index.js
+++ src/index.js
@@ -1,121 +1,134 @@
-class Super {
-    constructor() {
-        this.event = {};
-        this.eventOne = {};
-    }
-
-    _bind(str, fn, minNum = 1, event) {
-        let obj = event[str];
-        if (!obj) {
-            obj = [];
-            event[str] = obj;
+(function (name, factory) {
+    if (typeof exports === 'object' && typeof module !== 'undefined') { // nodejs - commonjs canon
+        module.exports = factory();
+    } else if (typeof define === 'function' && define.amd) { // requirejs - amd canon
+        define(factory);
+    } else if (window) { // window - browser canon
+        if (Object.prototype.toString.call(window.zhf).slice(8, -1).toLowerCase() !== 'Object') {
+            window.zhf = {};
         }
-        obj.push({
-            isDel: false,
-            minNum: minNum,
-            triggerNum: 0,
-            fn: fn,
-        });
+        window.zhf[name] = factory();
     }
-
-    _cancel(str, num, event) {
-        const obj = event[str];
-        if (obj) {
-            if (num && num >= 1) {
-                obj.splice((num - 1), 1, {
-                    isDel: true,
-                    fn: function () {
-                    },
-                });
-            } else {
-                obj.length = 0;
-            }
+})('EventEmitter', function () {
+    class EventEmitter {
+        constructor() {
+            this.event = {};
+            this.eventOne = {};
         }
-    }
 
-    _cancelAll(event) {
-        Object.keys(event).forEach((key) => {
-            event[key].length = 0;
-        });
-    }
-
-    _trigger(str, data, cb, event, type) {
-        const obj = event[str];
-        if (obj) {
-            if (!obj.allData) {
-                obj.allData = [];
+        _bind(str, fn, minNum = 1, event) {
+            let obj = event[str];
+            if (!obj) {
+                obj = [];
+                event[str] = obj;
             }
-            obj.allData.push(data);
-            obj.forEach((json) => {
-                json.triggerNum++;
-                if (json.triggerNum >= json.minNum && json.isDel === false) {
-                    json.fn({nowData: data, allData: obj.allData});
-                    // 销毁发布过的单次订阅
-                    if (type === 'once') {
-                        json.isDel = true;
-                        json.fn = function () {
-                        };
-                    }
-                }
-                if (Object.prototype.toString.call(cb).slice(8, -1).toLowerCase() === 'function') {
-                    json.data = data;
-                    cb(json);
-                }
+            obj.push({
+                isDel: false,
+                minNum: minNum,
+                triggerNum: 0,
+                fn: fn,
             });
-            // 销毁全部单次订阅
-            if (type === 'once') {
-                let isDelAll = true;
-                obj.forEach((json) => {
-                    if (json.isDel === false) {
-                        isDelAll = false;
-                    }
-                });
-                if (isDelAll) {
+        }
+
+        _cancel(str, num, event) {
+            const obj = event[str];
+            if (obj) {
+                if (num && num >= 1) {
+                    obj.splice((num - 1), 1, {
+                        isDel: true,
+                        fn: function () {
+                        },
+                    });
+                } else {
                     obj.length = 0;
                 }
             }
         }
-    }
 
-    // 订阅 minNum - 至少发布(emit)多少次才会发消息(on)给订阅者
-    on(str, fn, minNum) {
-        this._bind(str, fn, minNum, this.event);
-    }
+        _cancelAll(event) {
+            Object.keys(event).forEach((key) => {
+                event[key].length = 0;
+            });
+        }
 
-    // 单次订阅 minNum - 至少发布(emit)多少次才会发消息(on)给订阅者
-    once(str, fn, minNum) {
-        this._bind(str, fn, minNum, this.eventOne);
-    }
+        _trigger(str, data, cb, event, type) {
+            const obj = event[str];
+            if (obj) {
+                if (!obj.allData) {
+                    obj.allData = [];
+                }
+                obj.allData.push(data);
+                obj.forEach((json) => {
+                    json.triggerNum++;
+                    if (json.triggerNum >= json.minNum && json.isDel === false) {
+                        json.fn({nowData: data, allData: obj.allData});
+                        // 销毁发布过的单次订阅
+                        if (type === 'once') {
+                            json.isDel = true;
+                            json.fn = function () {
+                            };
+                        }
+                    }
+                    if (Object.prototype.toString.call(cb).slice(8, -1).toLowerCase() === 'function') {
+                        json.data = data;
+                        cb(json);
+                    }
+                });
+                // 销毁全部单次订阅
+                if (type === 'once') {
+                    let isDelAll = true;
+                    obj.forEach((json) => {
+                        if (json.isDel === false) {
+                            isDelAll = false;
+                        }
+                    });
+                    if (isDelAll) {
+                        obj.length = 0;
+                    }
+                }
+            }
+        }
 
-    // 取消订阅
-    off(str, num) {
-        if (str) {
-            this._cancel(str, num, this.event);
-            this._cancel(str, num, this.eventOne); // 取消单次订阅
-        } else {
-            this._cancelAll(this.event);
-            this._cancelAll(this.eventOne); // 取消全部单次订阅
+        // 订阅 minNum - 至少发布(emit)多少次才会发消息(on)给订阅者
+        on(str, fn, minNum) {
+            this._bind(str, fn, minNum, this.event);
+        }
+
+        // 单次订阅 minNum - 至少发布(emit)多少次才会发消息(on)给订阅者
+        once(str, fn, minNum) {
+            this._bind(str, fn, minNum, this.eventOne);
+        }
+
+        // 取消订阅
+        off(str, num) {
+            if (str) {
+                this._cancel(str, num, this.event);
+                this._cancel(str, num, this.eventOne); // 取消单次订阅
+            } else {
+                this._cancelAll(this.event);
+                this._cancelAll(this.eventOne); // 取消全部单次订阅
+            }
+        }
+
+        // 发布
+        emit(str, data, cb) {
+            this._trigger(str, data, cb, this.event);
+            this._trigger(str, data, cb, this.eventOne, 'once'); // 发布单次订阅并销毁
+        }
+
+        // 数组转对象
+        arrToJson(arr) {
+            const json = {};
+            arr.forEach(function (v, i) {
+                if (v.name) {
+                    json[v.name] = v;
+                } else {
+                    json[i] = v;
+                }
+            });
+            return json;
         }
     }
 
-    // 发布
-    emit(str, data, cb) {
-        this._trigger(str, data, cb, this.event);
-        this._trigger(str, data, cb, this.eventOne, 'once'); // 发布单次订阅并销毁
-    }
-
-    // 数组转对象
-    arrToJson(arr) {
-        const json = {};
-        arr.forEach(function (v, i) {
-            if (v.name) {
-                json[v.name] = v;
-            } else {
-                json[i] = v;
-            }
-        });
-        return json;
-    }
-}
-
-module.exports = Super;
+    return EventEmitter;
+});
